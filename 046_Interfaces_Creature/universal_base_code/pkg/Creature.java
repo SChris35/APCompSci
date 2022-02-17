a5e3cc1558116685edb738317d2e96f69ae03465
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:17:14 -0800

    Update and rename BaseClass.java to Creature.java

---
+++ 046_Interfaces_Creature/universal_base_code/pkg/Creature.java
@@ -0,0 +1,15 @@
+package pkg;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public interface Creature{
+
+boolean isDead();
+boolean takeDamage(int damage);
+String getName();
+void attack();
+
+
+}
+
