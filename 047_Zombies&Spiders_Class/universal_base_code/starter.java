99e04653ba6396703afeef39e9aabcb659d33c08
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:17:55 -0800

    Update starter.java

--- 047_Zombies&Spiders_Class/universal_base_code/starter.java
+++ 047_Zombies&Spiders_Class/universal_base_code/starter.java
@@ -1,4 +1,4 @@
-import pkg.*;
+iimport pkg.*;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -6,7 +6,26 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		Creature [] role = new Creature[3];
+    role[0] = new Zombie();
+    role[1] = new Spider();
+    role[2] = new Man();
+    int counter = 0;
+    
+    while(true)
+    {
+      if(role[1].isDead())
+      {
+        break;
+      }
+      if(role[2].isDead())
+      {
+        break;
+      }
+      role[2].attack(role[0]);
+      role[2].attack(role[1]);
+
+    }
 
 
 		
