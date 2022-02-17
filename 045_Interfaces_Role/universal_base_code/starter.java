478033411f47306b20bda193427a950a98f70e1c
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:14:23 -0800

    Update starter.java

--- 045_Interfaces_Role/universal_base_code/starter.java
+++ 045_Interfaces_Role/universal_base_code/starter.java
@@ -6,7 +6,18 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		
+    Role [] role = new Role [3];
+    role[0] = new Wizard();
+    role[1] = new Warrior();
+    role[2] = new Kermit();
+    
+    System.out.println(role[0].getName());
+    System.out.println(role[1].getName());
+    System.out.println(role[2].getName());
+
+    
+
 
 
 		
