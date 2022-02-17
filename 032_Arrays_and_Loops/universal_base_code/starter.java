526b683c7eddc44649be1aa231d0ed95bcded378
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:04:35 -0800

    Update starter.java

--- 032_Arrays_and_Loops/universal_base_code/starter.java
+++ 032_Arrays_and_Loops/universal_base_code/starter.java
@@ -6,9 +6,23 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+    Random rand = new Random();
 
+		int [] array = new int [10];
 
-		
-	}
+    int counter = 0;
+
+    while(counter < array.length)
+    {
+      array[counter] = rand.nextInt(1000);
+      counter++;
+    }
+    counter = 0;
+    while(counter < array.length)
+    {
+      System.out.println(array[counter]);
+      counter++;
+    }
+
+}
 }
