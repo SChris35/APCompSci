4737df129023120e845613d9205ed150a72470ac
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:07:52 -0800

    Update starter.java

--- 038_String_Methods_More/universal_base_code/starter.java
+++ 038_String_Methods_More/universal_base_code/starter.java
@@ -6,9 +6,20 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+    Scanner sc = new Scanner(System.in);
+
+		System.out.println("What is your first and last name? ");
+    String word = sc.nextLine();
+    int wordlocation;
+    
+   wordlocation = word.indexOf(" ");
+   //System.out.println(wordlocation);
+
+   System.out.println("Your Last name is: " + word.substring(wordlocation + 1));
+   
+   
 
 
-		
+    
 	}
 }
