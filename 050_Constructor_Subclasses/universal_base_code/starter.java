4ede2b4bbeea7abb9346f4b933489c0a6bf12aaf
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:22:26 -0800

    Update starter.java

--- 050_Constructor_Subclasses/universal_base_code/starter.java
+++ 050_Constructor_Subclasses/universal_base_code/starter.java
@@ -6,9 +6,15 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		Musician m1 = new Musician("Kenny");
+    m1.getName();
+    m1.practice();
+    m1.getInstrument();
 
-
+    Musician m2 = new Musician("Joe", 30, "saxophone");
+    m2.getName();
+    m2.practice();
+    m2.getInstrument();
 		
 	}
 }
