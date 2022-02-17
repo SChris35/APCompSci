79b8cad3a74cca5dd3169cca154677818293373d
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:02:56 -0800

    Update starter.java

--- 029_Arrays/universal_base_code/starter.java
+++ 029_Arrays/universal_base_code/starter.java
@@ -2,13 +2,39 @@ import pkg.*;
 import java.util.Scanner;
 import java.util.Random;
 
-
 class starter {
+  public static String Potato(String a)
+  {
+    int counter = 0;
+    while(counter < a.length())
+    {
+      System.out.println(a.substring(counter, counter+1));
+
+      counter++;
+    }
+
+
+    return a;
+  }
+ 
+  public static boolean Tomato(String a)
+  {
+    
+
+    return a;
+  }
+
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+    
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
 
+    System.out.println("Hello please enter a word ");
+    String word = sc.nextLine();
 
+    Potato(word);
+    System.out.println("Is this your favorite word?");    
 		
 	}
 }
