a235de7bd3dd1bc7f21621aff151e40317be7aff
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:04:11 -0800

    Update starter.java

--- 031_while_loop_review/universal_base_code/starter.java
+++ 031_while_loop_review/universal_base_code/starter.java
@@ -6,8 +6,21 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+    Random rand = new Random();
+    Scanner sc = new Scanner(System.in);
 
+    
+    System.out.println("Hello lets make a list of random numbers, all I need from you is a number between 1-100: ");
+    int num = sc.nextInt();
+
+
+
+    for(int counter = 1; counter < num; counter++)
+    {
+      int randnum = rand.nextInt(100);
+      System.out.println(randnum);
+      
+    }
 
 		
 	}
