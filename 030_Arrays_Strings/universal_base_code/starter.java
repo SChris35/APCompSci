a2330311660a048886c4bf4161a21c0fe0e72b95
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:03:35 -0800

    Update starter.java

--- 030_Arrays_Strings/universal_base_code/starter.java
+++ 030_Arrays_Strings/universal_base_code/starter.java
@@ -6,9 +6,21 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		Random rand = new Random();
+    
 
-
-		
+    String [] bob = new String [7];
+    bob[0] = "She told me don't worry about it..";
+    bob[2] = "She told me don't worry, no more";
+    bob[3] = "we both know we can't go without it...";
+    bob[4] = "she told me you'll never be in love ov ov... ahhh";
+    bob[5] = "I can't feel my face when I'm with you";
+		bob[6] = "But I love it";
+    int counter = 0;
+    while(counter < bob.length)
+    {
+      System.out.println(bob[counter]);
+      counter++;
+    }
 	}
 }
