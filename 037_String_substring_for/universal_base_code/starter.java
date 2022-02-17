c293fe6860e3724b6ffb161d0d957ece283c96fb
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:07:14 -0800

    Update starter.java

--- 037_String_substring_for/universal_base_code/starter.java
+++ 037_String_substring_for/universal_base_code/starter.java
@@ -6,7 +6,19 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+    Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter a word: ");
+    String word = sc.nextLine();
+    System.out.println("Letter by Letter: ");
+    
+    int wordlength = word.length();
+   
+    for(int cnt = 0; cnt < wordlength; cnt++)
+    {
+      System.out.print(cnt + " ");
+      System.out.println(word.substring(cnt, cnt+1));
+    }
 
 
 		
