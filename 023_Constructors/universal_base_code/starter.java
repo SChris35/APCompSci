c27e2193ad8afd4be311db96388c06c85d8bea13
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 17:01:03 -0800

    Update starter.java

--- 023_Constructors/universal_base_code/starter.java
+++ 023_Constructors/universal_base_code/starter.java
@@ -1,12 +1,23 @@
 import pkg.*;
 import java.util.Scanner;
 import java.util.Random;
+public class Character{
+String role;
+public Character(){
+   role = "No Role";
 
+}
+public Character(String a){
+role = a;
+
+}
+
+}
 
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-		BaseClass test = new BaseClass();
+		
 
 
 		
