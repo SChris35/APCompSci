0ffdb41156500bf2251ccd17c0ce679efbbe5e38
SChris35 <84989701+SChris35@users.noreply.github.com>
2022-02-16 16:59:38 -0800

    Update starter.java

--- 021_Classes/base_code/starter.java
+++ 021_Classes/base_code/starter.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
 import java.util.Random;
 
+ class Character{
+  String role = "Wizard";
+  int Strength = 0;
+  int Dex = 0;
+
+
+}
+
+
 class starter {
+
 	public static void main(String args[]) {
 		// Your code goes below here
-
+    Character hero = new Character();
+    System.out.println(hero.role);
 
 
 		
